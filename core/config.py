0381c6c8b97387d2f4d1c6516bd3108f8f294158
sean-modawell <sean.modawell@gmail.com>
2026-07-07 22:19:57 -0600

    sync flow successful. deploying async for testing

--- core/config.py
+++ core/config.py
@@ -20,7 +20,7 @@ import json
 from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
 
 # --- Logging Settings ---
-logging.basicConfig(level=logging.WARNING) # DEBUG > INFO > WARNING > ERROR > CRITICAL
+logging.basicConfig(level=logging.INFO) # DEBUG > INFO > WARNING > ERROR > CRITICAL
 logger = logging.getLogger(__name__)
 
 # --- Load Environment Variables ---
